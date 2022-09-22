30f33d2352b3948bceaabbbeac8fc6bdbd7a82b7
Ryan Fahey <rkfahey100@hotmail.com>
2022-09-21 20:55:25 -0400

    headers

--- bot/modules/genius/client.py
+++ bot/modules/genius/client.py
@@ -18,12 +18,26 @@ class GeniusAPIClient:
     # This function is internal to the class
     # It is NOT required to be called from outside the class
     async def _get_data(self, page: int, per_page: int) -> dict:
+        headers = {
+            'Host': 'genius.com',
+            'Connection': 'keep-alive',
+            'sec-ch-ua': '"Microsoft Edge";v="105", " Not;A Brand";v="99", "Chromium";v="105"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42',
+            'sec-ch-ua-platform': '"macOS"',
+            'Accept': '*/*',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://genius.com/',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
         params = {
             "page": page,
             "per_page": per_page,
         }
         logger.debug(f"Sending GET request to Genius API | Parameters: {str(params)}")
-        r = await self.session.get(GENIUS_API_URL, params=params)
+        r = await self.session.get(GENIUS_API_URL, params=params, headers=headers)
         logger.debug(
             f"Received response from Genius API | Status: {r.status_code} | Response: {r.text}"
         )
